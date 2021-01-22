46fe5033d54c3d015d756ce2840977280ffc3866
sweeneyc22 <70588351+sweeneyc22@users.noreply.github.com>
2021-01-22 10:27:50 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main extends arrayClass {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double a[] = createArray(s);
+        displaySortedNumbers(a);
+    }
+}
